586cf28cc4e2e9780d558c6b146d61fb4db5c12c
Florian Theron <mikyan0207@gmail.com>
2022-12-08 10:13:06 +0100

    Create UE5 project

---
+++ Source/RacingPods/RacingPods.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RacingPods.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RacingPods, "RacingPods" );
---
+++ Source/RacingPods/RacingPodsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RacingPodsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACINGPODS_API ARacingPodsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
